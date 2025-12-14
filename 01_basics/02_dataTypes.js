4d220fd3f4bf0ee01f42fa5762b869594078ecc7
Mohitkaushik0 <mohitkaushik92811@gmail.com>
2025-12-14 17:16:53 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+/* javascript dataTypes = 2 Types
+
+1. Primitive Datatypes (simple value) 
+2. Non-Primitive (Reference) Datatypes
+
+
+Primitive Datatypes (7)
+
+1. Number                              2.   String                   5. NULL 
+number (integer + decimal)                Text/ Words                    let a = null;
+Example                                   Example                                    
+let a = 10;                               let name = "mohit"          6. Biglnt
+let b = 3.5;                              let city = 'delhi';            bahut bada number 
+                                                                     let big = 4864546546545684456n;
+
+3. BOOLEAN                              4. undefine                   7. Symbol
+true & false                                value assign nhi ki            unique value
+                                             let x;                        let id = symbol("id");
+
+
+
+Non-Primitive Datatypes 
+
+ 1. object 
+let student = {
+name = "Aman",
+ age = 25
+ };
+
+2. Array 
+list of values 
+
+3. Function 
+function add(a, b) {
+    return a+ b;
+    }
+
+    */
